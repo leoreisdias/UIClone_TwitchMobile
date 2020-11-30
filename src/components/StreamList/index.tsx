c6b93aac9e16f29f2fc0d50649aa9defba979018
leoreisdias <leonardord99@gmail.com>
2020-11-30 18:58:43 -0300

    twitch clone

---
+++ src/components/StreamList/index.tsx
@@ -0,0 +1,65 @@
+import React from 'react';
+
+import streamThumbnail from '../../images/stream_thumbnail.jpg';
+
+import {
+  List,
+  StreamContainer,
+  StreamRow,
+  StreamThumbnail,
+  StreamColumn,
+  StreamHeader,
+  StreamAvatar,
+  StreamUsername,
+  StreamDescription,
+  StreamCategory,
+  TagRow,
+  TagView,
+  TagText
+} from './styles';
+
+const StreamList: React.FC = () => {
+  const StreamItem = () => (
+    <StreamContainer>
+      <StreamThumbnail source={streamThumbnail} />
+
+      <StreamColumn>
+        <StreamRow>
+          <StreamHeader>
+            <StreamAvatar />
+            <StreamUsername numberOfLines={1}>Kumoleo</StreamUsername>
+          </StreamHeader>
+
+          <StreamDescription numberOfLines={1}>
+            AQUELE QUE ENXERGA TUDO — JOGANDO NO PS5
+            Live até cansar
+          </StreamDescription>
+
+          <StreamCategory numberOfLines={1}>
+            Ação e Aventura
+          </StreamCategory>
+        </StreamRow>
+
+        <TagRow>
+          <TagView>
+            <TagText>Portuguese</TagText>
+          </TagView>
+          <TagView>
+            <TagText>Aventura</TagText>
+          </TagView>
+        </TagRow>
+      </StreamColumn>
+    </StreamContainer>
+  )
+
+  return (
+    <List>
+      <StreamItem />
+      <StreamItem />
+      <StreamItem />
+      <StreamItem />
+    </List>
+  );
+};
+
+export default StreamList;
